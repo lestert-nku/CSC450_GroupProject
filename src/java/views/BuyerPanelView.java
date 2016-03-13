24c9baafdfbb3c4d1e02dacbc213700a42d38aa1
Timothy Lester <timoby86@gmail.com>
2016-03-13 16:34:01 -0400

    Added JSplitPane to BuyerPanelView and started work on adding controls.
    Added JSplitPane to BuyerPanelView and started work on adding controls.

--- src/java/views/BuyerPanelView.java
+++ src/java/views/BuyerPanelView.java
@@ -4,14 +4,53 @@ import java.awt.*;
 import javax.swing.*;
 
 public class BuyerPanelView extends BasePanelView{
+    private JPanel searchParamPanel;
+    private JList searchResultList;
+    private JTextField paramCityText;
+    private JTextField paramZipText;
 
     BuyerPanelView() {}
 
     @Override
     protected void configureUI(){
-        panel.setBackground(new Color(0,200,0));
-        panel.setLayout(new FlowLayout());
-        JLabel testLabel = new JLabel("HELLO Buyer Panel!", JLabel.CENTER);
-        panel.add(testLabel);
+        this.searchParamPanel = new JPanel();
+        this.searchResultList = new JList();
+
+        // Create and set parameter layout
+        GridBagLayout paramGridLayout = new GridBagLayout();
+        this.searchParamPanel.setLayout(paramGridLayout);
+
+        // Initialize parameter search controls and add to searchParamPanel
+        this.paramCityText = new JTextField(10);
+        this.paramZipText = new JTextField(10);
+
+        this.searchParamPanel.add(new JLabel("City"), this.makeGbc(0,0));
+        this.searchParamPanel.add(this.paramCityText, this.makeGbc(1,0));
+        this.searchParamPanel.add(new JLabel("Zip"), this.makeGbc(0,1));
+        this.searchParamPanel.add(this.paramZipText, this.makeGbc(1,1));
+
+        JPanel testPanel = new JPanel(new FlowLayout());
+        testPanel.add(this.searchParamPanel);
+
+        JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, testPanel, this.searchResultList);
+        splitPane.setDividerLocation(200);
+
+        // Setup base JPanel
+        this.panel.setLayout(new BorderLayout());
+        this.panel.add(splitPane);
+    }
+
+    private GridBagConstraints makeGbc(int x, int y) {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridwidth = 1;
+        gbc.gridheight = 1;
+        gbc.gridx = x;
+        gbc.gridy = y;
+        gbc.weightx = x;
+        gbc.weighty = 1.0;
+        gbc.insets = new Insets(5, 5, 0, 5);
+        gbc.anchor = (x == 0) ? GridBagConstraints.LINE_START : GridBagConstraints.LINE_END;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        return gbc;
     }
 }
