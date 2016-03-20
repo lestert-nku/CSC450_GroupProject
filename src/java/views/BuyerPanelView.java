d61171f8bd88430725818b27178231115473bc53
Timothy Lester <timoby86@gmail.com>
2016-03-20 16:31:27 -0400

    Added back button, search button, and more param controls to BuyerPanelView.
    Added back button, search button, and more param controls to
    BuyerPanelView.

--- src/java/views/BuyerPanelView.java
+++ src/java/views/BuyerPanelView.java
@@ -1,11 +1,15 @@
 package edu.nku.csc450.views;
 
+import edu.nku.csc450.*;
 import java.awt.*;
+import java.awt.event.*;
 import javax.swing.*;
 
 public class BuyerPanelView extends BasePanelView{
     private JPanel searchParamPanel;
     private JList searchResultList;
+    private JButton searchButton;
+    private JButton backButton;
     private JTextField paramMinPrice;
     private JTextField paramMaxPrice;
     private JTextField paramCityText;
@@ -13,6 +17,11 @@ public class BuyerPanelView extends BasePanelView{
     private JTextField paramZipText;
     private JTextField paramBedroomText;
     private JTextField paramBathroomText;
+    private JTextField paramAcresText;
+    private JCheckBox paramBasementCheck;
+    private JCheckBox paramPoolCheck;
+    private JCheckBox paramCentralAir;
+    private JCheckBox paramGasHeat;
 
     public BuyerPanelView() {}
 
@@ -22,10 +31,16 @@ public class BuyerPanelView extends BasePanelView{
         this.searchResultList = new JList();
 
         // Create and set parameter layout
-        GridBagLayout paramGridLayout = new GridBagLayout();
-        this.searchParamPanel.setLayout(paramGridLayout);
+        this.searchParamPanel.setLayout(new GridBagLayout());
 
         // Initialize parameter search controls and add to searchParamPanel
+        this.searchButton = new JButton("Search");
+        this.backButton = new JButton("Back");
+        this.backButton.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                PanelController.showCard("MAIN");
+            }
+        });
         this.paramMinPrice = new JTextField(10);
         this.paramMaxPrice = new JTextField(10);
         this.paramCityText = new JTextField(10);
@@ -33,6 +48,11 @@ public class BuyerPanelView extends BasePanelView{
         this.paramZipText = new JTextField(10);
         this.paramBedroomText = new JTextField(10);
         this.paramBathroomText = new JTextField(10);
+        this.paramAcresText = new JTextField(10);
+        this.paramBasementCheck = new JCheckBox();
+        this.paramPoolCheck = new JCheckBox();
+        this.paramCentralAir = new JCheckBox();
+        this.paramGasHeat = new JCheckBox();
 
         // Add the search paramters to the layout
         this.searchParamPanel.add(new JLabel("Min Price"), this.makeGbc(0,0));
@@ -50,12 +70,24 @@ public class BuyerPanelView extends BasePanelView{
         this.searchParamPanel.add(this.paramBedroomText, this.makeGbc(1,6));
         this.searchParamPanel.add(new JLabel("Baths"), this.makeGbc(0,7));
         this.searchParamPanel.add(this.paramBathroomText, this.makeGbc(1,7));
+        this.searchParamPanel.add(new JLabel("Acres"), this.makeGbc(0,8));
+        this.searchParamPanel.add(this.paramAcresText, this.makeGbc(1,8));
+        this.searchParamPanel.add(new JLabel("Basement"), this.makeGbc(0,9));
+        this.searchParamPanel.add(this.paramBasementCheck, this.makeGbc(1,9));
+        this.searchParamPanel.add(new JLabel("Pool"), this.makeGbc(0,10));
+        this.searchParamPanel.add(this.paramPoolCheck, this.makeGbc(1,10));
+        this.searchParamPanel.add(new JLabel("Central Air"), this.makeGbc(0,11));
+        this.searchParamPanel.add(this.paramCentralAir, this.makeGbc(1,11));
+        this.searchParamPanel.add(new JLabel("Gas Heat"), this.makeGbc(0,12));
+        this.searchParamPanel.add(this.paramGasHeat, this.makeGbc(1,12));
+        this.searchParamPanel.add(this.backButton, this.makeGbc(0,20));
+        this.searchParamPanel.add(this.searchButton, this.makeGbc(1,20));
 
         JPanel innerParamPanel = new JPanel(new FlowLayout());
         innerParamPanel.add(this.searchParamPanel);
 
         JSplitPane splitPane = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, innerParamPanel, this.searchResultList);
-        splitPane.setDividerLocation(225);
+        splitPane.setDividerLocation(250);
 
         // Setup base JPanel
         this.panel.setLayout(new BorderLayout());
