70d76b57bd3f9a83877e244998d2c513e3a161b2
kakewauso <jdreyer.uso@gmail.com>
2016-04-22 11:58:05 -0400

    updated state array
    updated state array

--- src/java/views/BuyerPanelView.java
+++ src/java/views/BuyerPanelView.java
@@ -128,7 +128,7 @@ public class BuyerPanelView extends BasePanelView{
     }
 
     private String[] getStates(){
-        String[] states = {"", "IN", "KY", "OH"};
+	String[] states = {"","AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID", "IL","IN","KS","KY","LA","MA","MD","ME","MH","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY", "OH","OK","OR","PA","PR","PW","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"};
         return states;
     }
 
--- src/java/views/UpdatePanelView.java
+++ src/java/views/UpdatePanelView.java
@@ -130,7 +130,8 @@ public class UpdatePanelView extends JPanel{
     }
 
     private String[] getStates(){
-        return new String[] {"IN", "KY", "OH"};
+	String[] states = {"","AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID", "IL","IN","KS","KY","LA","MA","MD","ME","MH","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY", "OH","OK","OR","PA","PR","PW","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY"};
+        return states;
     }
 
     private String[] getStandardComboOptions(){
